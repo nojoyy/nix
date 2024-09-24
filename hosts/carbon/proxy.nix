205335c636a6aa92cab352893c95f7dea8446ce1
Noah <njoydev@proton.me>
2024-09-23 20:55:14 -0400

    updated proxy for nginx

--- hosts/carbon/proxy.nix
+++ hosts/carbon/proxy.nix
@@ -5,18 +5,45 @@
     enable = true;
 
     virtualHosts = {
+      # gitea instance
       "git.noahjoyner.com" = {
-        listen = [ { addr = "0.0.0.0"; port = 80; } ];
-        serverName = "git.example.com";
+        enableACME = true;
+        forceSSL = true;
         locations."/" = {
-          proxyPass = "http://localhost:3000";
           extraConfig = ''
-            proxy_set_header Host $host;
-            proxy_set_header X-Real-IP $remote_addr;
-            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-            proxy_set_header X-Forwarded-Proto $scheme;
-          '';  
+  proxy_set_header Host $host;
+  proxy_set_header X-Real-IP $remote_addr;
+  proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+  proxy_set_header X-Forwarded-Proto $scheme;
+          '';
+          proxyPass = "http://localhost:3000";
         };
+      };
+
+      # webpage
+      "www.noahjoyner.com" = {
+        enableACME = true;   # Automatically manage SSL (using Let's Encrypt).
+        forceSSL = true;     # Redirect HTTP to HTTPS.
+        
+        root = "/home/noah/www/default/";
+        
+        # Serve static files
+        locations."/".extraConfig = ''
+          try_files $uri $uri/ =404;
+        '';
+      };
+    };
+  };
+
+  security.acme = {
+    acceptTerms = true;
+    defaults.email = "njoydev@proton.me";
+    certs = {
+      "git.noahjoyner.com" = {
+        domain = "git.noahjoyner.com";
+      };
+      "www.noahjoyner.com" = {
+        domain = "www.noahjoyner.com";
       };
     };
   };
