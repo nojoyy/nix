b9a5e3df82e7fcbf38e9770cb9ac6b329bbe9aad
Noah <njoydev@proton.me>
2024-09-28 18:52:21 -0400

    (carbon) Removed dud database stuff for jellyfin

--- nixosModules/services/jellyfin.nix
+++ nixosModules/services/jellyfin.nix
@@ -9,26 +9,9 @@ in {
   };
 
   config = lib.mkIf module.enable {
-    services.postgre = {
-      enable = lib.mkDefault true;
-      initialScript = pkgs.writeScript ''
-        CREATE USER jellyfin WITH PASSWORD jellyPass;
-        CREATE DATABASE jellyfin WITH OWNER jellyfin;
-      '';
-    };
-
     services.jellyfin = {
       enable = lib.mkDefault true;
       openFirewall = true;
-
-      # Database config
-      database = {
-        type = "postgresql";
-        server = "localhost";
-        databse = "jellyfin";
-        user = "jellyfin";
-        password = "jellyPass";
-      };
     };
 
     services.sonarr = {
